1bd815cad2d0f2167a48fbfe74f4fb2659403d15
RubenGonz <ruben.gonzalez.rodriguez00@gmail.com>
2026-01-19 01:45:45 +0000

    Sección 8 - Aplicación de Monitoreo - NOC

---
+++ 05-NOC/src/presentation/server.ts
@@ -0,0 +1,20 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CronService } from "./cron/cron-service";
+
+export class Server {
+  static start() {
+    console.log("Server started...");
+
+    CronService.createJob(
+      "*/3 * * * * *",
+      () => {
+        const url = "https://google.com"
+
+        new CheckService(
+          () => console.log(`${url} is ok`),
+          (error) => console.log(error),
+        ).execute(url)
+      }
+    )
+  }
+}
