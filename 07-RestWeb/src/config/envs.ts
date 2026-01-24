784a2fab824ff1f3bd96dddd5f40a755b48b5440
RubenGonz <ruben.gonzalez.rodriguez00@gmail.com>
2026-01-24 17:00:35 +0000

    Add RestWeb as submodule

--- 07-RestWeb/src/config/envs.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config"
-
-import * as env from 'env-var';
-
-export const envs = {
-  PORT: env.get('PORT').required().asPortNumber(),
-  PUBLIC_PATH: env.get('PUBLIC_PATH').default("public").asString(),
-}
